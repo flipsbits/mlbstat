4667c22dfd05508ed26335c61f3eba5efeb5b2aa
Andre Guerlain <apguerlain@gmail.com>
2019-04-06 13:19:42 -0400

    Add initial python bits

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mlbstat',
+      version='0.1.0',
+      description='Client library for the MLB statsapi',
+      url='https://github.com/dreamforth/mlbstat',
+      author='Andre Guerlain',
+      author_email='apguerlain+dreamforth@gmail.com',
+      license='MIT',
+      packages=['mlbstat'],
+      zip_safe=False)
