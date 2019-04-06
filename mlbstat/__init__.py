95d20a58ee39ab5a6c410827e5439eb0b6f0996e
Andre Guerlain <apguerlain@gmail.com>
2019-04-06 19:06:27 -0400

    Add schedule API teamId parameter, rework parameters

--- mlbstat/__init__.py
+++ mlbstat/__init__.py
@@ -1,20 +1,27 @@
 BASE_URL = 'https://statsapi.mlb.com/api/'
 V_1_0_URL = BASE_URL + 'v1/'
 V_1_1_URL = BASE_URL + 'v1.1/'
-SCHEDULE_URL = V_1_0_URL + 'schedule?sportId=1&date={}&language=en'
-GAME_URL = V_1_1_URL + 'game/{}/feed/live?language=en'
+SCHEDULE_URL = V_1_0_URL + 'schedule'
+GAME_URL = V_1_1_URL + 'game/{}/feed/live'
+
+# Default parameters
 
 import requests
 import datetime
 
-def schedule(query_datetime=None):
-    if not query_datetime:
-        query_datetime = datetime.date.today()
-    r = requests.get(SCHEDULE_URL.format(query_datetime))
+def schedule(query_datetime=None,teamId=None):
+    parameters = {'sportId': '1', 'language': 'en'}
+    if query_datetime:
+        parameters['date'] = datetime.date.today()
+    if teamId:
+        parameters['teamId'] = str(teamId)
+
+    r = requests.get(SCHEDULE_URL, parameters)
     r.raise_for_status()
     return r.json()
 
 def game(gameId):
-    r = requests.get(GAME_URL.format(gameId))
+    parameters = {'language': 'en'}
+    r = requests.get(GAME_URL.format(gameId), parameters)
     r.raise_for_status()
     return r.json()
